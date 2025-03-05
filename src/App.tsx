1492f362db7cb0234a8c1f683cba87311447c77c
Miftahul Aziz <140588199+azizsiberu@users.noreply.github.com>
2025-03-05 15:27:04 +0700

    Add src/App.tsx 1741163224480

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { CssBaseline } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CssBaseline, Box } from '@mui/material';
 import Header from './layout/Header';
 import Sidebar from './layout/Sidebar';
 import Footer from './layout/Footer';
 import ProtectedRoute from './routes/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
 
 const App: React.FC = () => {
+  const userRole = useSelector((state: RootState) => state.user?.role);
+
   return (
     <BrowserRouter>
       <CssBaseline />
-      <Header />
-      <Sidebar />
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute
-                component={Dashboard}
+      <div className="app">
+        <Header />
+        <div className="main-container">
+          {userRole && <Sidebar />}
+          <Box component="main" sx={{ flexGrow: 1, p: 3, bgcolor: 'background.default' }}>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
                 path="/"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
               />
-            }
-          />
-        </Routes>
-      </main>
-      <Footer />
+            </Routes>
+          </Box>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
